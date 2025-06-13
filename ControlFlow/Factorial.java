354967f447f844367d104d8ae42bceb81bb6995e
PD-001 <110840658+PD-001@users.noreply.github.com>
2025-06-13 09:10:33 +0530

    Solution to factorial problem

---
+++ ControlFlow/Factorial.java
@@ -0,0 +1,21 @@
+package ControlFlow;
+
+import java.util.Scanner;
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new java.util.Scanner(System.in);
+        System.out.print("Enter a positive integer to find its factorial: ");
+        int number = sc.nextInt();
+        sc.close();
+
+        long factorial = 1;
+        int i = 1;
+
+        while (i <= number) {
+            factorial *= i;
+            i++;
+        }
+
+        System.out.println("The factorial of " + number + " is " + factorial);
+    }
+}
