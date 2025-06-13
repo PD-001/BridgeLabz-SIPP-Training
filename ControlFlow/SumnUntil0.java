d81e0632242d78fbda3751608153ef6fecd30828
PD-001 <110840658+PD-001@users.noreply.github.com>
2025-06-13 09:12:34 +0530

    Prakash Dixit add. Solution to sum until zero problem

---
+++ ControlFlow/SumnUntil0.java
@@ -0,0 +1,20 @@
+package ControlFlow;
+import java.util.Scanner;
+public class SumnUntil0 {
+    public static void main(String[] args) {
+        Scanner sc = new java.util.Scanner(System.in);
+        System.out.print("Enter numbers to sum (0 to stop): ");
+        
+        int sum = 0;
+        int number;
+
+        do {
+            number = sc.nextInt();
+            sum += number;
+        } while (number != 0);
+
+        sc.close();
+        System.out.println("The total sum is: " + sum);
+    }
+    
+}
