7976426b7bca765d4d54151a84e83ae3ba57706b
PD-001 <110840658+PD-001@users.noreply.github.com>
2025-06-13 09:57:51 +0530

    Prakash Dixit add.Prints Sum of Natural Numbers

--- ControlFlow/SumNaturalNumberusingwhile.java
+++ ControlFlow/SumNaturalNumberusingwhile.java
@@ -2,15 +2,21 @@ package ControlFlow;
 
 import java.util.Scanner;
 
-public class SumNaturalNumber {
-    public static void main(String[] args) {
+public class SumNaturalNumberusingwhile {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = sc.nextInt();
         sc.close();
+        int sum1=0;
 
         if (number >= 0) {
             int sum = number * (number + 1) / 2;
+            while(number > 0) {
+                sum1 += number;
+                number--;
+            }
+            if(sum==sum1)
             System.out.println("The sum of " + number + " natural numbers is " + sum);
         } else {
             System.out.println("The number " + number + " is not a natural number");
