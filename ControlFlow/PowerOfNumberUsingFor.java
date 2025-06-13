37815082acf15b0e3cc92cc1f4b210bf0410b08b
PD-001 <110840658+PD-001@users.noreply.github.com>
2025-06-13 10:28:11 +0530

    Prakash Dixit add.Finds power of a number

--- ControlFlow/PowerOfNumberUsingFor.java
+++ ControlFlow/PowerOfNumberUsingFor.java
@@ -1,6 +1,6 @@
 package ControlFlow;
 import java.util.Scanner;  
-public class PowerOfNumber {
+public class PowerOfNumberUsingFor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
