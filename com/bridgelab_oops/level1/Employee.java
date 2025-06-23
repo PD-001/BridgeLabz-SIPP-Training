23ebcaeddf6ff70b7f04cd8669a12b0003c8fdf0
PD-001 <110840658+PD-001@users.noreply.github.com>
2025-06-23 11:37:23 +0530

    check.

---
+++ com/bridgelab_oops/level1/Employee.java
@@ -0,0 +1,12 @@
+package com.bridgelab_oops.level1;
+
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hell");
+
+	}
+
+}
