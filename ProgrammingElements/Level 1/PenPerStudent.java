4b2e93ee13a3f215940ce45668646a106aada9ff
PD-001 <110840658+PD-001@users.noreply.github.com>
2025-06-23 10:35:39 +0530

    CLEAN MAIN BRANCH.

--- ProgrammingElements/Level 1/PenPerStudent.java
+++
@@ -1,8 +0,0 @@
-public class PenPerStudent{
-    public static void main(String[] args) {
-        int pens = 14, students = 3;
-        int each = pens / students;
-        int remaining = pens % students;
-        System.out.println("The Pen Per Student is " + each + " and the remaining pen not distributed is " + remaining);
-    }
-}
