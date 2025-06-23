06c26067f4f07373e43ff0fe07d78111ef1180a5
PD-001 <110840658+PD-001@users.noreply.github.com>
2025-06-23 14:43:12 +0530

    [Prakah Dixit] Add. A program to check if a number is pallindrome.

---
+++ com/bridgelab_oops/level1/PallindromeCheckerMain.java
@@ -0,0 +1,17 @@
+package com.bridgelab_oops.level1;
+
+import java.util.Scanner;
+
+public class PallindromeCheckerMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string to check for palindrome: ");
+        String input = sc.nextLine();
+
+        PallindromeChecker checker = new PallindromeChecker(input);
+        checker.displayResult();
+
+        sc.close();
+    }
+}
