ed7ec4bcb25802bbc2443bb9f8b586df41db3c87
PD-001 <110840658+PD-001@users.noreply.github.com>
2025-06-23 12:47:55 +0530

    [Prakah Dixit] Refactor. A program to store and display details of Employees.

--- com/bridgelab_oops/level1/EmployeeMain.java
+++ com/bridgelab_oops/level1/EmployeeMain.java
@@ -5,23 +5,23 @@ import java.util.Scanner;
 public class EmployeeMain {
 
 	public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         // User input
         System.out.print("Enter Employee Name: ");
-        String name = scanner.nextLine();
+        String name = sc.nextLine();
 
         System.out.print("Enter Employee ID: ");
-        int id = scanner.nextInt();
+        int id = sc.nextInt();
 
         System.out.print("Enter Employee Salary: ");
-        double salary = scanner.nextDouble();
+        double salary = sc.nextDouble();
 
         // Create and display employee
         Employee emp = new Employee(name, id, salary);
         emp.displayDetails();
 
-        scanner.close();
+        sc.close();
     }
 
 }
