89137a7cd6f9637e8747a8f11d47f8745de7b194
PD-001 <110840658+PD-001@users.noreply.github.com>
2025-06-26 08:37:13 +0530

    [Prakash Dixit] Add. Implementation of a Person class with a copy constructor that clones another person's attributes.

---
+++ com/bridgelab_oops_part2/PersonMain.java
@@ -0,0 +1,15 @@
+package com.bridgelab_oops_part2;
+
+public class PersonMain {
+	public static void main(String[] args) {
+		// Create original person
+		Person person1 = new Person("Alice", 28);
+		person1.displayInfo();
+
+		System.out.println();
+
+		// using copy constructor
+		Person person2 = new Person(person1);
+		person2.displayInfo();
+	}
+}
