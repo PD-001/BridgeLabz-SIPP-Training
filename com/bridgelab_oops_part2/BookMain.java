579380b5ce57cbfe787ac7e5319ba1050a0e725e
PD-001 <110840658+PD-001@users.noreply.github.com>
2025-06-26 08:32:04 +0530

    [Prakash Dixit] Add. Implementation of a Book class with attributes title, author, and price. Provide both default and parameterized constructors.

---
+++ com/bridgelab_oops_part2/BookMain.java
@@ -0,0 +1,15 @@
+package com.bridgelab_oops_part2;
+
+public class BookMain {
+	public static void main(String[] args) {
+		// Using default constructor
+		Book defaultBook = new Book();
+		defaultBook.displayInfo();
+
+		System.out.println();
+
+		// Using parameterized constructor
+		Book customBook = new Book("XYZ", "ABC", 1499);
+		customBook.displayInfo();
+	}
+}
