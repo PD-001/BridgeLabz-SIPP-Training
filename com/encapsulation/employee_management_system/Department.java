c12ac94f525851b263a67be59b8fa84ec9fd1d27
PD-001 <110840658+PD-001@users.noreply.github.com>
2025-07-06 16:11:38 +0530

    [Prakash Dixit] Add. Solutions for problems covering topics Encapsulation, Polymorphism, Interface and Abstract Class.

--- com/encapsulation/employee_management_system/Department.java
+++
@@ -1,7 +0,0 @@
-package com.encapsulation.employee_management_system;
-
-//Interface representing a Department
-public interface Department {
- void assignDepartment(String deptName);
- String getDepartmentDetails();
-}
